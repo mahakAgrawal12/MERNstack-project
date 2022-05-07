fcf61779b8356dcce24dfe8449c617a3adde034e
ishasingh23 <isha.singh_cs19@gla.ac.in>
2022-05-07 19:18:12 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React ,{useEffect}from "react";
+import{Container,AppBar,Typography,Grow,Grid} from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {getPosts} from './actions/posts';
+import { useDispatch } from "react-redux";
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import PostAda from './images/mem.png';
+import useStyles from './styles';
+
+const App=()=>{
+    const classes=useStyles();
+    const dispatch=useDispatch();
+    useEffect(()=>{
+    dispatch(getPosts());
+    },[dispatch]);
+    
+
+    
+      return(
+         < Container maxwidth='lg'>
+         <AppBar className={classes.appBar} position="static" color="inherit">
+         <Typography className={classes.heading} variant="h2" align="center">
+         PostAda
+
+         </Typography >
+
+         <img className={classes.image} src={PostAda} alt="PostAda" height="60"/>
+         </AppBar>
+         <Grow in>
+         <Container>
+             <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
+<Grid item xs={12} sm={7}>
+<Posts />
+</Grid><Grid item xs={12} sm={4}>
+    <Form />
+</Grid>
+
+             </Grid>
+         </Container>
+
+         </Grow>
+         
+         </Container>
+      
+    );
 }
-
 export default App;
